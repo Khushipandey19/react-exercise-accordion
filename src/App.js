10f6993b96f84a6a3f9e235bbb4062910847747f
Khushi Pandey <67337373+Khushipandey19@users.noreply.github.com>
2025-07-02 20:25:44 +0000

    completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 const faqs = [
@@ -27,19 +28,31 @@ function Accordion({ data }) {
   return (
     <div>
       {data.map((item, index) => (
-        <AccordionItem title={item.title} text={item.text} num={index} />
+        <AccordionItem
+          title={item.title}
+          text={item.text}
+          num={index}
+          key={item.title}
+        />
       ))}
     </div>
   );
 }
 
 function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    console.log("called");
+    setIsOpen((isOpen) => !isOpen);
+  }
+
   return (
-    <div className="item">
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
-      <p className="text">{text}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{title}</div>
+      <p className="text">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
