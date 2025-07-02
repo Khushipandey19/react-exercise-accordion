6465d57aa3b7835628d368408b0a6d37d6fe4246
Khushipandey19 <67337373+Khushipandey19@users.noreply.github.com>
2025-07-03 01:39:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  return (
+    <div>
+      {data.map((item, index) => (
+        <AccordionItem title={item.title} text={item.text} num={index} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  return (
+    <div className="item">
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="text">{text}</p>
+      <p className="icon">-</p>
+      <div className="content-box">{title}</div>
+    </div>
+  );
+}
